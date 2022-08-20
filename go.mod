af3dc2a18cad29d79c1966bdf6e7a918627308df
Fabian Hahn <fabian@hahn.graphics>
2022-08-20 15:08:24 +0100

    hello world

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module raidlogscan
 
-go 1.13
+go 1.16
 
-require cloud.google.com/go/storage v1.25.0 // indirect
+require (
+	cloud.google.com/go/storage v1.25.0 // indirect
+	github.com/GoogleCloudPlatform/functions-framework-go v1.6.1
+)
---
+++ hello_test.go
@@ -0,0 +1,21 @@
+package raidlogscan
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHelloGet(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	helloGet(rr, req)
+
+	want := "Hello, World!"
+	if got := rr.Body.String(); got != want {
+		t.Errorf("helloGet() = %q, want %q", got, want)
+	}
+}
+
