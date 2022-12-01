08f774cd9c8c4c8a7f874bf170667e24b46134ae
Fabian Hahn <fabian@hahn.graphics>
2022-12-01 17:40:02 +0000

    retry events on failure

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,10 @@ gcloud functions deploy scanuserreports --gen2 --runtime=go116 --region=europe-w
 gcloud functions deploy scanguildreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=ScanGuildReports --trigger-http --allow-unauthenticated
 gcloud functions deploy scanrecentcharacterreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=ScanRecentCharacterReports --trigger-http --allow-unauthenticated
 
-gcloud functions deploy coraideraccountclaim --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=CoraiderAccountClaim --trigger-topic=coraideraccountclaim
-gcloud functions deploy reportaccountclaim --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=ReportAccountClaim --trigger-topic=reportaccountclaim
-gcloud functions deploy fetchguildreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchGuildReports --trigger-topic=guildreports
-gcloud functions deploy fetchreport2 --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchReport --trigger-topic=report
-gcloud functions deploy updateplayerreport --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=UpdatePlayerReport --trigger-topic=playerreport
-gcloud functions deploy fetchuserreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchUserReports --trigger-topic=userreports
-gcloud functions deploy fetchrecentcharacterreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchRecentCharacterReports --trigger-topic=recentcharacterreports
+gcloud functions deploy coraideraccountclaim --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=CoraiderAccountClaim --retry --trigger-topic=coraideraccountclaim
+gcloud functions deploy reportaccountclaim --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=ReportAccountClaim --retry --trigger-topic=reportaccountclaim
+gcloud functions deploy fetchguildreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchGuildReports --retry --trigger-topic=guildreports
+gcloud functions deploy fetchreport2 --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchReport --retry --trigger-topic=report
+gcloud functions deploy updateplayerreport --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=UpdatePlayerReport --retry --trigger-topic=playerreport
+gcloud functions deploy fetchuserreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchUserReports --retry --trigger-topic=userreports
+gcloud functions deploy fetchrecentcharacterreports --gen2 --runtime=go116 --region=europe-west2 --source=gs://raidlogscan_sources/source.zip --entry-point=FetchRecentCharacterReports --retry --trigger-topic=recentcharacterreports
